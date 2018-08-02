069b9d822a7574ba7bacdbe9b0ac493632eca08f
Liu Ye <879316283@qq.com>
2018-08-02 15:33:37 +0100

    人脸识别优化

--- Modules/FaceRecognition/FaceRecognition.py
+++ Modules/FaceRecognition/FaceRecognition.py
@@ -12,38 +12,39 @@ def CatchUsbVideo(window_name, camera_idx):
     classfier = cv2.CascadeClassifier(
         "/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_alt2.xml")
 
-    # 识别出人脸后要画的边框的颜色，RGB格式
+    # Frame color
     color = (0, 255, 0)
 
     while cap.isOpened():
-        ok, frame = cap.read()  # 读取一帧数据
+        ok, frame = cap.read()
         if not ok:
+            print('frame not found')
             break
 
-        # 将当前帧转换成灰度图像
+        # Create gray level image
         grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        # 人脸检测，1.2 和 2 分别为图片缩放比例和需要检测的有效点数
+        # Face recognition
         faceRects = classfier.detectMultiScale(
             grey, scaleFactor=1.2, minNeighbors=3, minSize=(32, 32))
-        if len(faceRects) > 0:  # 大于 0 则检测到人脸
-            for (x, y, w, h) in faceRects:  # 单独框出每一张人脸
+
+        if len(faceRects) > 0:
+            for (x, y, w, h) in faceRects:
                 cv2.rectangle(frame, (x - 10, y - 10),
                               (x + w + 10, y + h + 10), color, 2)
 
                 font = cv2.FONT_HERSHEY_SIMPLEX
-                cv2.putText(frame, '%d%%' % int(
-                    100 * h / frame.shape[0]), (x + 10, y + 30), font, 1, (255, 0, 255), 4)
+                cv2.putText(frame, 'Size: %d%%' % int(
+                    100 * h / frame.shape[0]), (x + 5, y + 30), font, 1, (255, 0, 255), 3)
 
-        # 显示图像
+        # Show image
         cv2.imshow(window_name, frame)
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
 
-    # 释放摄像头并销毁所有窗口
     cap.release()
     cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
-    CatchUsbVideo("FaceRecognition", 1)
+    CatchUsbVideo("FaceRecognition", 1)  # 0 for original camera, 1 for webcam
